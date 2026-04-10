057df5fb4ae9ca369d071767e94ff3772ce0531d
roderick correa <adrian.cb.soft1019@gmail.com>
2026-04-10 11:20:39 -0500

    fix: arreglo de la api

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -9,7 +9,7 @@ class ApiService {
         "title": "Camisa de algodón blanca",
         "description": "Camisa cómoda de algodón para uso diario",
         "category": "Camisas",
-        "image": "https://via.placeholder.com/300x300?text=Camisa+Blanca",
+        "image": "https://www.puntoblanco.co/cdn/shop/files/ceya-blanco-900-729513_000900-5_b3902d80-3983-4d9d-a25a-ac431b18de0d.jpg",
         "ratingCount": 120,
       },
       {
@@ -17,7 +17,7 @@ class ApiService {
         "title": "Camisa azul formal",
         "description": "Camisa azul perfecta para ocasiones formales",
         "category": "Camisas",
-        "image": "https://via.placeholder.com/300x300?text=Camisa+Azul",
+        "image": "https://thumbs.dreamstime.com/b/camisas-de-vestir-azules-colgadas-en-el-tendedero-una-camisa-azul-crujiente-y-limpia-cuelga-un-contra-fondo-blanco-perfecto-para-392358856.jpg",
         "ratingCount": 95,
       },
       {
@@ -25,7 +25,7 @@ class ApiService {
         "title": "Camisa a cuadros",
         "description": "Camisa casual con diseño a cuadros",
         "category": "Camisas",
-        "image": "https://via.placeholder.com/300x300?text=Camisa+Cuadros",
+        "image": "https://whitmanstore.com.co/cdn/shop/files/CamisaHCIJamaicaCuadrosVerdes1.jpg",
         "ratingCount": 60,
       },
       {
@@ -33,7 +33,7 @@ class ApiService {
         "title": "Pantalones vaqueros clásicos",
         "description": "Pantalones vaqueros resistentes y cómodos",
         "category": "Pantalones",
-        "image": "https://via.placeholder.com/300x300?text=Pantalones+Vaqueros",
+        "image": "https://arturocalle.vteximg.com.br/arquivos/ids/770615/HOMBRE-JEAN-10135898-AZUL-740_1.jpg",
         "ratingCount": 85,
       },
       {
@@ -41,7 +41,7 @@ class ApiService {
         "title": "Pantalones de vestir negros",
         "description": "Pantalones elegantes para el trabajo",
         "category": "Pantalones",
-        "image": "https://via.placeholder.com/300x300?text=Pantalones+Negros",
+        "image": "https://totalchef.mx/cdn/shop/files/Pantalon-Basico-Negro-Talla-Mediana-Uniformes-De-Mexico-PBN05.png",
         "ratingCount": 110,
       },
       {
@@ -49,7 +49,7 @@ class ApiService {
         "title": "Pantalones cargo",
         "description": "Pantalones con bolsillos para aventuras",
         "category": "Pantalones",
-        "image": "https://via.placeholder.com/300x300?text=Pantalones+Cargo",
+        "image": "https://colombia.bioweb.co/cdn/shop/products/ALFI_e90db195-755d-42f3-800a-94912e49b7de.jpg",
         "ratingCount": 95,
       },
       {
@@ -57,14 +57,14 @@ class ApiService {
         "title": "Zapatos deportivos running",
         "description": "Zapatos cómodos para correr y ejercicio",
         "category": "Zapatos",
-        "image": "https://via.placeholder.com/300x300?text=Zapatos+Running"
+        "image": "https://www.gef.co/cdn/shop/files/zoe-tenis-beige-8040-745739_008040-1_28a4d3dd-79ba-45ed-8e60-8d299f42a053.jpg"
       },
       {
         "id": 8,
         "title": "Zapatos formales negros",
         "description": "Zapatos clásicos para ocasiones especiales",
         "category": "Zapatos",
-        "image": "https://via.placeholder.com/300x300?text=Zapatos+Formales",
+        "image": "https://www.villaromana.com.co/cdn/shop/products/ZA00103031_01.jpg",
         "ratingCount": 150,
       },
       {
@@ -72,7 +72,7 @@ class ApiService {
         "title": "Abrigo de invierno grueso",
         "description": "Abrigo cálido para climas fríos",
         "category": "Abrigos",
-        "image": "https://via.placeholder.com/300x300?text=Abrigo+Invierno",
+        "image": "https://m.media-amazon.com/images/I/71vlmpcIe0L._AC_UY1000_.jpg",
         "ratingCount": 150,
       },
       {
@@ -80,7 +80,7 @@ class ApiService {
         "title": "Chaqueta ligera",
         "description": "Chaqueta perfecta para primavera",
         "category": "Abrigos",
-        "image": "https://via.placeholder.com/300x300?text=Chaqueta+Ligera",
+        "image": "https://m.media-amazon.com/images/I/61S2JEkSQJL._AC_UY1000_.jpg",
         "ratingCount": 120,
       },
       {
@@ -88,7 +88,7 @@ class ApiService {
         "title": "Vestido elegante rojo",
         "description": "Vestido perfecto para ocasiones especiales",
         "category": "Vestidos",
-        "image": "https://via.placeholder.com/300x300?text=Vestido+Rojo",
+        "image": "https://png.pngtree.com/png-vector/20250220/ourlarge/pngtree-elegant-red-satin-evening-gown-png-image_15539980.png",
         "ratingCount": 90,
       },
       {
@@ -96,7 +96,7 @@ class ApiService {
         "title": "Vestido casual azul",
         "description": "Vestido cómodo para uso diario",
         "category": "Vestidos",
-        "image": "https://via.placeholder.com/300x300?text=Vestido+Azul",
+        "image": "https://looktwice.co/cdn/shop/files/KNM005-1-Vestido-Azul-Polo-KNM005_e9b014b3-b73f-4539-8f15-79f177f1b99b.jpg",
         "ratingCount": 75,
       },
     ];
